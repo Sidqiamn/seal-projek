7a07a4227fe740b146a5d51f532551e88bbf43b0
Sidqi Amanullah <shidqiamanullah.sa@gmail.com>
2024-08-21 16:34:01 +0700

    add api'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,68 +9,75 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex justify-between mobile:justify-start fixed pt-4 top-0 z-0 w-full px-10 mobile:px-2 border-b pb-4 mobile:relative">
-      <div className="flex items-center gap-5 ">
+    <div className="flex justify-between mobile:justify-start fixed top-0 z-0 w-full px-10 mobile:px-2 border-b pb-4 mobile:relative">
+      <div className="flex items-center gap-5">
         <img src={logo} alt="Logo" className="animate-bounce" />
         <p className="animate-pulse">Berita Kini</p>
       </div>
       <div className="flex gap-12 items-center mobile:hidden">
-        <h5
+        <a
+          href="https://api-berita-indonesia.vercel.app/cnn/Beranda"
           className={`text-sm cursor-pointer ${
             klikNav === "beranda" ? "text-blue-500" : ""
           }`}
           onClick={() => handleNavBlue("beranda")}
         >
           Beranda
-        </h5>
-        <h5
+        </a>
+        <a
+          href="https://api-berita-indonesia.vercel.app/cnn/terbaru"
           className={`text-sm cursor-pointer ${
             klikNav === "Terbaru" ? "text-blue-500" : ""
           }`}
           onClick={() => handleNavBlue("Terbaru")}
         >
           Terbaru
-        </h5>
-        <h5
+        </a>
+        <a
+          href="https://api-berita-indonesia.vercel.app/cnn/hiburan"
           className={`text-sm cursor-pointer ${
             klikNav === "Hiburan" ? "text-blue-500" : ""
           }`}
           onClick={() => handleNavBlue("Hiburan")}
         >
           Hiburan
-        </h5>
-        <h5
+        </a>
+        <a
+          href="https://api-berita-indonesia.vercel.app/cnn/gayahidup"
           className={`text-sm cursor-pointer ${
             klikNav === "Gaya Hidup" ? "text-blue-500" : ""
           }`}
           onClick={() => handleNavBlue("Gaya Hidup")}
         >
           Gaya Hidup
-        </h5>
-        <h5
+        </a>
+        <a
+          href="https://api-berita-indonesia.vercel.app/cnn/olahraga"
           className={`text-sm cursor-pointer ${
             klikNav === "Olahraga" ? "text-blue-500" : ""
           }`}
           onClick={() => handleNavBlue("Olahraga")}
         >
           Olahraga
-        </h5>
-        <h5
+        </a>
+        <a
+          href="https://api-berita-indonesia.vercel.app/cnn/nasional"
           className={`text-sm cursor-pointer ${
             klikNav === "Nasional" ? "text-blue-500" : ""
           }`}
           onClick={() => handleNavBlue("Nasional")}
         >
           Nasional
-        </h5>
-        <h5
+        </a>
+        <a
+          href="https://api-berita-indonesia.vercel.app/cnn/internasional"
           className={`text-sm cursor-pointer ${
             klikNav === "Internasional" ? "text-blue-500" : ""
           }`}
           onClick={() => handleNavBlue("Internasional")}
         >
           Internasional
-        </h5>
+        </a>
       </div>
     </div>
   );
